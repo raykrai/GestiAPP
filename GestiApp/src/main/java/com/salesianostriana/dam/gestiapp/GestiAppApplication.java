f821b730a3304a8de79695d0cc1837f6ab33f67c
Jose Manuel Barguenño López <jmbarguenolopez@gmail.com>
2019-09-27 08:54:02 +0200

    Añadidos registros en base de datos

--- GestiApp/src/main/java/com/salesianostriana/dam/gestiapp/GestiAppApplication.java
+++ GestiApp/src/main/java/com/salesianostriana/dam/gestiapp/GestiAppApplication.java
@@ -17,52 +17,52 @@ public class GestiAppApplication {
 		
 	}
 	
-	@Bean
-    public CommandLineRunner init(AppUserService servicio, BCryptPasswordEncoder passwordEncoder) {
-        return args -> {
-
-        	AppUser adm = new AppUser();
-    		
-    		adm.setName("Manolo");
-    		adm.setSurname("Rodríguez");
-    		adm.setUserEmail("123@123.com");
-    		adm.setPassword("123");
-    		adm.setValidated(true);
-    		adm.setAdmin(true);
-
-            servicio.save(adm);
-
-            AppUser user = new AppUser();
-    		
-    		user.setName("Pepa");
-    		user.setSurname("García");
-    		user.setUserEmail("321@321.com");
-    		user.setPassword("321");
-    		user.setValidated(false);
-    		user.setAdmin(false);
-    		
-    		AppUser user2 = new AppUser();
-    		
-    		user2.setName("Pepa");
-    		user2.setSurname("García");
-    		user2.setUserEmail("44@44.com");
-    		user2.setPassword("444");
-    		user2.setValidated(false);
-    		user2.setAdmin(false);
-
-            servicio.save(user);
-            
-            servicio.save(user2);
-            
-            
-            for (AppUser usuario : servicio.findAll()) {
-                usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
-                servicio.save(usuario);
-            }
-
-        };
-	
-	}
+//	@Bean
+//    public CommandLineRunner init(AppUserService servicio, BCryptPasswordEncoder passwordEncoder) {
+//        return args -> {
+//
+//        	AppUser adm = new AppUser();
+//    		
+//    		adm.setName("Manolo");
+//    		adm.setSurname("Rodríguez");
+//    		adm.setUserEmail("123@123.com");
+//    		adm.setPassword("123");
+//    		adm.setValidated(true);
+//    		adm.setAdmin(true);
+//
+//            servicio.save(adm);
+//
+//            AppUser user = new AppUser();
+//    		
+//    		user.setName("Pepa");
+//    		user.setSurname("García");
+//    		user.setUserEmail("321@321.com");
+//    		user.setPassword("321");
+//    		user.setValidated(false);
+//    		user.setAdmin(false);
+//    		
+//    		AppUser user2 = new AppUser();
+//    		
+//    		user2.setName("Pepa");
+//    		user2.setSurname("García");
+//    		user2.setUserEmail("44@44.com");
+//    		user2.setPassword("444");
+//    		user2.setValidated(false);
+//    		user2.setAdmin(false);
+//
+//            servicio.save(user);
+//            
+//            servicio.save(user2);
+//            
+//            
+//            for (AppUser usuario : servicio.findAll()) {
+//                usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+//                servicio.save(usuario);
+//            }
+//
+//        };
+//	
+//	}
 	
 		
 };
