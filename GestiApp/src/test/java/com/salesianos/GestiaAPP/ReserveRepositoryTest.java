8949d68db9b8bfc4acb182240a73221913392395
jesus ceacero <cads918@gmail.com>
2019-10-29 19:45:27 +0100

    cosas

--- GestiApp/src/test/java/com/salesianos/GestiaAPP/ReserveRepositoryTest.java
+++ GestiApp/src/test/java/com/salesianos/GestiaAPP/ReserveRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.salesianos.GestiaAPP;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.time.LocalDate;
 import java.util.List;
