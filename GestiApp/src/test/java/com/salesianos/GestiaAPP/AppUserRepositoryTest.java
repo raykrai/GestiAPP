671e4087965075dae5e90f49a2d4247bd3355956
myrows <milenio204@hotmail.com>
2019-10-29 17:37:05 +0100

    Mi test

---
+++ GestiApp/src/test/java/com/salesianos/GestiaAPP/AppUserRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.salesianos.GestiaAPP;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.salesianostriana.dam.gestiapp.model.AppUser;
+import com.salesianostriana.dam.gestiapp.repository.AppUserRepository;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ActiveProfiles("test")
+class AppUserRepositoryTest {
+
+	@Autowired
+	TestEntityManager entityManager;
+	
+	@Autowired
+	AppUserRepository appUserRepository;
+	
+	@Test
+	public void testEncontrarPrimeroPorEmail() {
+		
+		AppUser a= new AppUser();
+		a.setName("Daniel");
+		a.setSurname("Troncoso Rubio");
+		a.setPassword("1234");
+		a.setUserEmail("buenas@gmail.com");
+		a.setAdmin(true);
+		a.setValidated(false);
+		a.setReserveList(null);
+		a.setSchool(null);
+		entityManager.persist(a);
+		
+		AppUser user = appUserRepository.findFirstByUserEmail(a.getUserEmail());
+		
+		assertThat(user.getName()).isEqualTo("Daniel");
+		
+	}
+
+}
